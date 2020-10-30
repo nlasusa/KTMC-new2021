e3698feb7415622134ed5ce8bb8d89bfc54af604
Nicole Lasusa <nicolelasusa@Nicoles-Air.fios-router.home>
2020-10-30 13:21:07 -0400

    all content first push

---
+++ assets/javascript/insights.js
@@ -0,0 +1,30 @@
+ /* //////// ****** /////////////////////////// */
+ // MONTHLY INSIGHTS SECTION 
+   // ON CLICK EVENTS 
+   $("#imgi1").on('click', function(){
+    window.location = "http://ktmc.com/webfiles/AprilMonthlyInsights2020%20(4).pdf";    
+  });
+  $("#imgi2").on('click', function(){
+    window.location = "http://ktmc.com/files/23186_ktmc_bulletin_fall_l.pdf";    
+  });
+  $("#imgi3").on('click', function(){
+    window.location = "http://ktmc.com/webfiles/KTMC%20Firm%20Brochure.PDF";    
+  });
+  $("#imgi4").on('click', function(){
+    window.location = "http://ktmc.com/webfiles/KTMC%20SecuritiesTracker%20Brochure.pdf";    
+  });
+  $("#imgi5").on('click', function(){
+    window.location = "http://ktmc.com/webfiles/PRIMER%20ON%20SHAREHOLDER%20LIT.pdf";    
+  });
+  $("#imgi6").on('click', function(){
+    window.location = "https://ktmc.com/webfiles/Benchmark20.pdf";    
+  });
+  $("#imgi7").on('click', function(){
+    window.location = "http://ktmc.com/webfiles/AprilMonthlyInsights2020%20(4).pdf";    
+  });
+  $("#imgi8").on('click', function(){
+    window.location = "http://ktmc.com/webfiles/AprilMonthlyInsights2020%20(4).pdf";    
+  });
+ // END OF MONTHLY INSIGHTS SECTION 
+  /* //////// ****** /////////////////////////// */
+  
---
+++ assets/javascript/navbar.js
@@ -0,0 +1,111 @@
+// BEGINNING OF TESTIMONIALS DROPDOWN NAVIGATION 
+$("#dropdown-testimonials").on('click', function(){
+    window.location = "testimonials.html";    
+  });
+// END OF TESTIMONIALS DROPDOWN NAVIGATION 
+
+
+// HOME BUTTON NAVIGATION 
+$("#dropdown-home").on('click', function(){
+    window.location = "index.html";    
+  });
+// END OF HOME BUTTON NAVIGATION 
+
+
+// FAQS BUTTON NAVIGATION 
+$("#dropdown-FAQs").on('click', function(){
+  window.location = "FAQs.html";    
+});
+// END OF FAQS BUTTON NAVIGATION 
+
+
+// IN THE COMMUNITY BUTTON NAVIGATION 
+$("#dropdown-community").on('click', function(){
+  window.location = "community.html";    
+});
+// END OF IN THE COMMUNITY BUTTON NAVIGATION 
+
+
+// INSIGHTS BUTTON NAVIGATION 
+$("#dropdown-insights").on('click', function(){
+  window.location = "insights.html";    
+});
+// END OF INSIGHTS BUTTON NAVIGATION 
+
+
+// AWARDS & ACHIEVEMENTS BUTTON NAVIGATION 
+$("#dropdown-awards").on('click', function(){
+  window.location = "awardsachievements.html";    
+});
+// END OF AWARDS & ACHIEVEMENTS BUTTON NAVIGATION 
+
+
+// NEWS BUTTON NAVIGATION 
+$("#dropdown-news").on('click', function(){
+  window.location = "news.html";    
+});
+
+$("#nav-news").on('click', function(){
+  window.location = "news.html";    
+});
+
+$(".view-more-news").on('click', function(){
+  window.location = "news.html";    
+});
+// END OF NEWS BUTTON NAVIGATION 
+
+
+// OFFICES BUTTON NAVIGATION 
+$("#dropdown-offices").on('click', function(){
+  window.location = "offices.html";    
+});
+// END OF OFFICES BUTTON NAVIGATION 
+
+
+// OFFICES BUTTON NAVIGATION 
+$("#dropdown-sustainability").on('click', function(){
+  window.location = "sustainability.html";    
+});
+// END OF OFFICES BUTTON NAVIGATION 
+
+
+// OVERVIEW BUTTON NAVIGATION 
+$("#dropdown-overview").on('click', function(){
+  window.location = "overview.html";    
+});
+// END OF OVERVIEW BUTTON NAVIGATION 
+
+
+// CONFERENCES BUTTON NAVIGATION 
+$("#dropdown-conferences").on('click', function(){
+  window.location = "conferences.html";    
+});
+// END OF CONFERENCES BUTTON NAVIGATION 
+
+
+// US SECURITIES (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
+$("#dropdown-USsecurities").on('click', function(){
+  window.location = "USsecurities.html";    
+});
+// END OF US SECURITIES (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
+
+
+// GLOBAL SECURITIES (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
+$("#dropdown-globalsecurities").on('click', function(){
+  window.location = "globalsecurities.html";    
+});
+// GLOBAL SECURITIES (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
+
+
+// CORPORATE GOVERNANCE (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
+$("#dropdown-corporategov").on('click', function(){
+  window.location = "corporategov.html";    
+});
+// CORPORATE GOVERNANCE (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
+
+
+// CORPORATE GOVERNANCE (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
+$("#about-us-read").on('click', function(){
+  window.location = "overview.html";    
+});
+// CORPORATE GOVERNANCE (UNDER PRACTICE AREAS) BUTTON NAVIGATION 
